192e3344c6749521c9374558511414a33d231563
Norman Institorisz <normaninstitorisz11@gmail.com>
2019-09-12 16:00:29 +0100

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,10 @@
+const add = (a, b) => a + b;
+const substract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a - b;
+
+function operate (operator, a, b) {
+    if(operator ==  '+') {
+        add(a, b)
+    }
+} 
