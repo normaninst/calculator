59a48b271ba0b275c019e80ac2b60d44ad8067e0
Norman Institorisz <normaninstitorisz11@gmail.com>
2019-09-25 21:18:12 +0100

    First commit

--- javascript.js
+++ javascript.js
@@ -1,10 +1,21 @@
+function operate (operator, a, b) {
+    switch (operator) {
+        case '+':
+            return add(a,b);
+            break;
+        case '-':
+            return substract(a, b);
+            break;
+        case '*':
+            return multiply(a, b);
+            break;
+        case ':':
+            return divide(a, b);
+            break;
+    }
+}
+ 
 const add = (a, b) => a + b;
 const substract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
-const divide = (a, b) => a - b;
-
-function operate (operator, a, b) {
-    if(operator ==  '+') {
-        add(a, b)
-    }
-} 
+const divide = (a, b) => a / b;
